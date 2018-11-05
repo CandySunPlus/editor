db78e78d23fe7f8d14544fb9c259ae545860a5f3
Fengming Sun <s@sfmblog.cn>
2018-11-05 15:22:25 +0800

    fix

--- src/formats/block-image.ts
+++ src/formats/block-image.ts
@@ -2,4 +2,5 @@ import Image from 'quill/formats/image';
 
 export default class extends Image {
     static className = 'ql-block-image';
+    static blotName = 'block-image';
 }
--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,6 @@ class Editor {
         this.quill = new Quill(mountDom, {
             placeholder: 'placeholder...'
         });
-        console.log(this.quill);
     }
 
     bindAction(selector: string, action: ActionType) {
@@ -41,15 +40,24 @@ class Editor {
         // add video
         this.quill.insertEmbed(0, 'video', url, 'api');
     }
+
+    getContent() {
+        return this.quill.getContents();
+    }
 }
 
 const editor = new Editor('.editor-container');
 const addImageBtn = document.querySelector('.add-image-btn');
 const addVideoBtn = document.querySelector('.add-video-btn');
+const getContentBtn = document.querySelector('.get-content-btn');
 
 addImageBtn.addEventListener('click', () => {
     editor.addImage('https://statics.wehaowu.com/13210159056683009/2bba8687-eeae-44c5-a398-df9f2176f968.JPG');
 });
 addVideoBtn.addEventListener('click', () => {
     editor.addVideo('https://v.statics.wehaowu.com/13210159056683009/ccef0316-e700-45a8-8c7b-0ea82ac9fcb7.mp4');
+});
+getContentBtn.addEventListener('click', () => {
+    const content = editor.getContent();
+    console.log(content);
 });
